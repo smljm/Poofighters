2ac9788b67f700153bb6c86eae9a1fe78dcac0d6
dlwlsahdlqslek101110rnrmf-dev <dlwlsahdlqslek101110rnrmf@gmail.com>
2026-05-05 22:24:27 +0900

    Update Ground.cs

--- Assets/Scripts/Ground.cs
+++ Assets/Scripts/Ground.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class GroundAnchor : MonoBehaviour
 {
-    public float heightFromBottom = 0.5f; // 바닥에서 얼마나 위에
+    public float heightFromBottom = 0.5f;
 
     void Start()
     {
@@ -10,8 +10,7 @@ public class GroundAnchor : MonoBehaviour
         float bottomY = cam.transform.position.y - cam.orthographicSize;
         float groundY = bottomY + heightFromBottom;
         transform.position = new Vector3(transform.position.x, groundY, transform.position.z);
-
-        // X 스케일도 화면 너비에 맞춤
+        
         float screenWidth = cam.orthographicSize * 2f * cam.aspect;
         transform.localScale = new Vector3(screenWidth, 1f, 1f);
     }
